462c7c6abd1f447b67046e9510d26950c74f0873
Gaspar Mari <gaspar.mari@hotmail.com>
2023-12-03 21:50:19 -0300

    se agrega el proyecto

---
+++ main.js
@@ -0,0 +1,29 @@
+class Producto {
+
+    constructor(nombre, precio, stock) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+}
+
+
+function renderizarProductos(productos) {
+
+    S
+}
+
+
+
+
+const listadoDeProductos = [
+
+    new Producto ("Harry Potter", 9000, 8),
+    new Producto ("Los juegos del hambre", 12000, 2),
+    new Producto ("El hobbit", 15000, 20),
+    new Producto ("1984", 8000, 11),
+    new Producto ("El gran gatsby", 6000, 15),
+    new Producto ("Los juegos del hambre", 7000, 9)
+];
+
